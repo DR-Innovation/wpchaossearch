a8a1b28069d419b31e666fbabbf45eb5db638d3f
Kræn Hansen (for DR A&R Innovation) <kh@bitblueprint.com>
2013-09-30 17:28:57 +0200

    Fixing markup updates in the basic search-form.

--- templates/search-form.php
+++ templates/search-form.php
@@ -4,8 +4,13 @@
  * @version 1.0
  */
 ?>
-<form method="GET" action="<?php echo $page; ?>" class="span12">
-	<div class="input-append">
-		<input class="span11" id="appendedInputButton" type="text" name="<?php echo WPChaosSearch::QUERY_KEY_FREETEXT; ?>" value="<?php echo WPChaosSearch::get_search_var(WPChaosSearch::QUERY_KEY_FREETEXT, 'esc_attr'); ?>" placeholder="<?php echo $freetext_placeholder; ?>" /><button type="submit" class="btn btn-large btn-search"><?php __ex('Search','verb','wpchaossearch'); ?></button>
+<form method="GET" action="<?php echo $page; ?>" class="col-12">
+	<div class="col-12 input-group">
+		<input type="hidden" name="<?php echo WPChaosSearch::QUERY_KEY_VIEW; ?>" value="<?php echo WPChaosSearch::get_search_var(WPChaosSearch::QUERY_KEY_VIEW, 'esc_attr'); ?>">
+		<input type="hidden" name="<?php echo WPChaosSearch::QUERY_KEY_SORT; ?>" value="<?php echo WPChaosSearch::get_search_var(WPChaosSearch::QUERY_KEY_SORT, 'esc_attr'); ?>">
+		<input class="form-control" id="appendedInputButton" type="text" name="<?php echo WPChaosSearch::QUERY_KEY_FREETEXT; ?>" value="<?php echo WPChaosSearch::get_search_var(WPChaosSearch::QUERY_KEY_FREETEXT, 'esc_attr,trim'); ?>" placeholder="<?php echo $freetext_placeholder; ?>" />
+		<span class="input-group-btn">
+			<button type="submit" class="btn btn-search btn-large" id="searchsubmit"><?php _ex('Search','verb','dka'); ?></button>
+		</span>
 	</div>
 </form>
